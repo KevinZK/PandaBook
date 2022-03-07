d1bd034c13be870f29306f78395a7a27dbc20635
Kevin <515867748@qq.com>
2022-03-07 14:09:30 +0800

    feat：add Wave Progress View

--- CowBookkeeper/Views/CBDebtView.swift
+++ CowBookkeeper/Views/CBDebtView.swift
@@ -14,19 +14,27 @@ struct CBDebtView: View {
     
     
     var body: some View {
-        ZStack{
-            WaveView(color: .blue.opacity(0.5), amplify: 150, isReverse: false)
-            WaveView(color: .blue.opacity(0.6), amplify: 100, isReverse: true)
-        }
-        .ignoresSafeArea(.all, edges: .bottom)
-        
-            
-//        NavigationView {
-//            ScrollView(.vertical) {
-//                WaveView(color: .blue, amplify: 150)
-//            }
-//            .navigationTitle(AppModuleType.debt.title)
+//        ZStack{
+//            WaveView(color: AppModuleType.debt.bgColor, isReverse: false, radius: 80, progress: 0.56)
 //        }
+//        .ignoresSafeArea(.all, edges: .bottom)
+        
+            ScrollView(.vertical) {
+                VStack {
+                    Text("￥35600.6")
+                        .font(.system(size: 50))
+                        .bold()
+                        .padding(.trailing, 40)
+                        .foregroundColor(.white)
+                    Text("总" + AppModuleType.debt.title)
+                        .foregroundColor(.white)
+                }
+                .frame(width: ScreenW*0.93, height: 120)
+                .background(Color(hex: 0x073B4C))
+                .cornerRadius(20)
+                
+            }
+            .navigationTitle(AppModuleType.debt.title)
     }
     
 }
@@ -39,8 +47,30 @@ struct CBDebtView_Previews: PreviewProvider {
 
 struct WaveView: View {
     var color: Color
-    var amplify: CGFloat
     var isReverse: Bool
+    var radius: CGFloat
+    var progress: CGFloat
+    
+    var body: some View {
+        ZStack {
+            WavePathView(color: color, isReverse: false, radius: radius, progress: progress)
+            Text("\(progress * 100, specifier: "%.1f")%")
+                .bold()
+        }
+        .frame(width: radius * 2, height: radius * 2)
+        .cornerRadius(radius)
+        .overlay {
+            Circle().stroke(.gray.opacity(0.2), lineWidth: 1)
+        }
+    }
+    
+}
+
+struct WavePathView: View {
+    var color: Color
+    var isReverse: Bool
+    var radius: CGFloat
+    var progress: CGFloat
     
     var body: some View {
         TimelineView(.animation) { timeLine in
@@ -51,28 +81,26 @@ struct WaveView: View {
                 
                 let offset = angle * size.width
                 
-//                context.draw(Text("angle:\(angle),offset:\(offset)"), at: CGPoint(x: size.width / 2, y: 100))
-                
                 context.translateBy(x: isReverse ? -offset : offset, y: 0)
-                context.fill(path(size: size), with: .color(color))
+                context.fill(path(size: size), with: .linearGradient(Gradient(colors: [.white, color]), startPoint: CGPoint(x: size.width/2, y: size.height), endPoint: CGPoint(x: size.width/2, y: 0.0)))
                 context.translateBy(x: -size.width, y: 0)
-                context.fill(path(size: size), with: .color(color))
+                context.fill(path(size: size), with: .linearGradient(Gradient(colors: [.white, color]), startPoint: CGPoint(x: size.width/2, y: size.height), endPoint: CGPoint(x: size.width/2, y: 0.0)))
                 context.translateBy(x: size.width * 2, y: 0)
-                context.fill(path(size: size), with: .color(color))
+                context.fill(path(size: size), with: .linearGradient(Gradient(colors: [.white, color]), startPoint: CGPoint(x: size.width/2, y: size.height), endPoint: CGPoint(x: size.width/2, y: 0.0)))
                 
             }
-            .frame(width: 400, height: 400)
-//            .cornerRadius(40)
+            
         }
     }
     
     func path(size: CGSize) -> Path {
+        let amplify = radius / 2.0
         return Path { path in
-            let midHeight = size.height / 2
+            let midHeight = size.height * (1 - progress)
             let width = size.width
-            
+            let offset = progress <= 0.5 ? progress/0.5 : (1 - progress)/0.5
             path.move(to: CGPoint(x: 0, y: midHeight))
-            path.addCurve(to: CGPoint(x: width, y: midHeight), control1: CGPoint(x: width * 0.5, y: midHeight + amplify), control2: CGPoint(x: width * 0.5, y: midHeight - amplify))
+            path.addCurve(to: CGPoint(x: width, y: midHeight), control1: CGPoint(x: width * 0.5, y: midHeight + amplify * offset), control2: CGPoint(x: width * 0.5, y: midHeight - amplify * offset))
             path.addLine(to: CGPoint(x: width, y: size.height))
             path.addLine(to: CGPoint(x: 0, y: size.height))
         }
